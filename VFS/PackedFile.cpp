407e60b349f49de565b81c15e839136936110482
Mingzhe Zhou <zhoumingzhe@gmail.com>
2012-12-11 13:46:50 +0800

    read packed file

--- VFS/PackedFile.cpp
+++ VFS/PackedFile.cpp
@@ -4,13 +4,15 @@
 #include "UnpackedFile.h"
 
 PackedFile::PackedFile(UnpackedFile* pFile, int size):
-    m_pFile(pFile),m_Size(size)
+    m_pFile(pFile),m_Size(size),m_Buffer(4*1024)
 {
     m_Stream.zalloc = Z_NULL;
     m_Stream.zfree = Z_NULL;
     m_Stream.opaque = Z_NULL;
     m_Stream.avail_in = 0;
     m_Stream.next_in = Z_NULL;
+    m_Stream.avail_out = 0;
+    m_Stream.next_out = 0;
     int ret = inflateInit(&m_Stream);
     assert(ret == Z_OK);
 }
@@ -23,38 +25,27 @@ PackedFile::~PackedFile(void)
 
 int PackedFile::Read(void* buffer, int size)
 {
-    if(size < (int)m_Buffer.size())
-    {
-        memcpy(buffer, &m_Buffer[0], size);
-        for(int i = 0; i<(int)m_Buffer.size() - size; ++i)
-        {
-            m_Buffer[i] = m_Buffer[i+size];
-        }
-        m_Buffer.resize(m_Buffer.size() - size);
-        return size;
-    }
-    if(m_Buffer.size() > 0)
-        memcpy(buffer, &m_Buffer[0], m_Buffer.size());
+    if(size<=0)
+        return 0;
 
-    int size_left = size - m_Buffer.size();
-    buffer =(char*)buffer + m_Buffer.size();
-
-    m_Buffer.resize(0);
-
-    std::vector<unsigned char> data;
-    data.resize(4*1024);
-    int result;
-    m_Stream.avail_out = size_left;
+    m_Stream.avail_out = size;
     m_Stream.next_out = (unsigned char*)buffer;
-    do
-    {
-        m_Stream.avail_in = m_pFile->Read(&data[0], data.size());
-        m_Stream.next_in = &data[0];
-        result = inflate(&m_Stream, Z_NO_FLUSH);
-    }
-    while(result != Z_STREAM_END && m_Stream.avail_out>0);
 
-    return 0;
+    while(m_Stream.avail_out>0)
+    {
+        if(m_Stream.avail_in<=0)
+        {
+            m_Stream.avail_in = m_pFile->Read(&m_Buffer[0], m_Buffer.size());
+            m_Stream.next_in = &m_Buffer[0];
+        }
+        if(m_Stream.avail_in<=0)
+            break;
+        int result = inflate(&m_Stream, Z_NO_FLUSH);
+        if(result != Z_OK)
+            break;
+    }
+
+    return m_Stream.next_out - (unsigned char*)buffer;
 }
 int PackedFile::Write(const void* buffer, int size)
 {
